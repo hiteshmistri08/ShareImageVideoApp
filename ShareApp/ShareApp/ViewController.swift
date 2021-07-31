d973b9a484d9af89a3cdb79d6fe6bec34dfc7860
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2021-07-31 15:43:59 +0530

    - Create iOS App from Xcode Template

---
+++ ShareApp/ShareApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShareApp
+//
+//  Created by Hitesh on 31/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
